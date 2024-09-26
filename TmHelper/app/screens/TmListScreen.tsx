27a6ef156353fe2af42cf385c93dfebeb5b6e329
Gavin Li <76128837+nihowdyy@users.noreply.github.com>
2024-09-26 13:49:27 -0400

    Added Functionality to add pokemon move types to the list screen

--- TmHelper/app/screens/TmListScreen.tsx
+++ TmHelper/app/screens/TmListScreen.tsx
@@ -1,10 +1,12 @@
 // screens/HomeScreen.tsx
 
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Pressable } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Pressable, Image} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import moveTypes from '../../assets/images/moveTypes';
 const tmData = require('../../assets/json/PKMN-SV-TMS.json');
 
+// TM Data Information
 interface TMData {
   tm_info: {
     number: string;
@@ -19,13 +21,43 @@ interface TMData {
   };
 }
 
+type MoveType = 
+  | "Bug"
+  | "Dark"
+  | "Dragon"
+  | "Electric"
+  | "Fairy"
+  | "Fighting"
+  | "Fire"
+  | "Flying"
+  | "Ghost"
+  | "Grass"
+  | "Ground"
+  | "Ice"
+  | "Normal"
+  | "Poison"
+  | "Psychic"
+  | "Rock"
+  | "Steel"
+  | "Water";
+
 const data: TMData[] = tmData as TMData[];
 
-const RenderItem = React.memo(({ item, onPress }: { item: TMData; onPress: (item: TMData) => void }) => (
-  <Pressable onPress={() => onPress(item)} style={styles.item}>
-    <Text style={styles.title}>TM {item.tm_info.number}: {item.tm_info.name}</Text>
-  </Pressable>
-));
+const RenderItem = React.memo(({ item, onPress }: { item: TMData; onPress: (item: TMData) => void }) => {
+  // Get the SVG image corresponding to the move type
+  const moveTypeImage = moveTypes[item.move_info.type as MoveType] || moveTypes['Normal']; // Fallback to 'Normal' if the type is not found
+
+  return (
+    <Pressable onPress={() => onPress(item)} style={styles.item}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Text style={styles.title}>
+          TM {item.tm_info.number}: {item.tm_info.name}
+        </Text>
+        <Image source={moveTypeImage} style={styles.image} />
+      </View>
+    </Pressable>
+  );
+});
 
 const HomeScreen = ({ navigation }: any) => {
   const handlePress = React.useCallback((tm: TMData) => {
@@ -55,6 +87,11 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
     pointerEvents: 'auto',
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
   },
   title: {
     fontSize: 18,
---
+++ TmHelper/assets/images/moveTypes.js
@@ -0,0 +1,21 @@
+const moveTypes = {
+    "Bug": require("../../assets/images/types/Bug.png"),
+    "Dark": require("../../assets/images/types/Dark.png"),
+    "Dragon": require("../../assets/images/types/Dragon.png"),
+    "Electric": require("../../assets/images/types/Electric.png"),
+    "Fairy": require("../../assets/images/types/Fairy.png"),
+    "Fighting": require("../../assets/images/types/Fighting.png"),
+    "Fire": require("../../assets/images/types/Fire.png"),
+    "Flying": require("../../assets/images/types/Flying.png"),
+    "Ghost": require("../../assets/images/types/Ghost.png"),
+    "Grass": require("../../assets/images/types/Grass.png"),
+    "Ground": require("../../assets/images/types/Ground.png"),
+    "Ice": require("../../assets/images/types/Ice.png"),
+    "Normal": require("../../assets/images/types/Normal.png"),
+    "Poison": require("../../assets/images/types/Poison.png"),
+    "Psychic": require("../../assets/images/types/Psychic.png"),
+    "Rock": require("../../assets/images/types/Rock.png"),
+    "Steel": require("../../assets/images/types/Steel.png"),
+    "Water": require("../../assets/images/types/Water.png"),
+}
+export default moveTypes;
