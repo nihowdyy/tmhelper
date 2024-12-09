4154a4d664416176124322c4d7140d2ab946b9c1
Gavin Li <76128837+nihowdyy@users.noreply.github.com>
2024-12-09 18:04:26 -0500

    Implemented a Pokemon Learnset Dropdown

--- TmHelper/app/screens/PokeDetails.tsx
+++ TmHelper/app/screens/PokeDetails.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, ScrollView } from 'react-native';
+import Collapsible from 'react-native-collapsible';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 // Resources
 import pokemonTypes from '../../assets/images/moveTypes';
@@ -28,7 +30,6 @@ type Types =
   | 'Steel'
   | 'Water';
 
-// Define types for props
 type PokemonDexEntry = {
   dex_info: {
     type: string;
@@ -36,6 +37,9 @@ type PokemonDexEntry = {
   };
   pokemon_info: {
     name: string;
+    abilities: string[];
+    type: Types[];
+    stats: { [key: string]: number };
   };
 };
 
@@ -48,19 +52,18 @@ type BarProps = {
   statName: string;
   statValue: number;
   barColor: string;
-  backgroundColor: string;
 };
 
-const StatBar = ({ statName, statValue, barColor, backgroundColor }: BarProps) => {
+const StatBar = ({ statName, statValue, barColor }: BarProps) => {
   const maxStat = 255;
   const barWidth = `${(statValue / maxStat) * 100}%`;
 
-  const shortStat = statName.slice(0, 3)
+  const shortStat = statName.slice(0, 3);
 
   return (
     <View style={styles.statRow}>
       <Text style={styles.statName}>{shortStat}</Text>
-      <View style={[styles.barContainer, { backgroundColor }]}>
+      <View style={styles.barContainer}>
         <View style={[styles.statBar, { width: barWidth, backgroundColor: barColor }]} />
       </View>
       <Text style={styles.statValue}>{statValue}</Text>
@@ -71,18 +74,34 @@ const StatBar = ({ statName, statValue, barColor, backgroundColor }: BarProps) =
 const PokeDetails = ({ route }: any) => {
   const { pokemon } = route.params; // Retrieve the pokemon data passed through navigation
 
-  const stats = pokemon.pokemon_info.stats;
+  const [isCollapsed, setIsCollapsed] = useState(true); // Tracking Collapsible State
 
-  // Pokemon Dex Number Component
+  // Filter data to match the current Pokémon's name
+  const filteredMoveData = moveData.filter((entry: any) => entry.name === pokemon.pokemon_info.name);
+
+  // Flatten the learnset into an array of { level, move }
+  const flattenedMoves = filteredMoveData[0].learnset.level_up.map(item => {
+    const [level, move] = item.split(': '); // Splitting each level and move pair
+    return { level: level, move: move };
+  });
+
+  const stats = pokemon.pokemon_info.stats; // Specific Pokemon Base stat information
+
+  const barData = [
+    { statName: 'HP', barColor: '#69DC12' },
+    { statName: 'ATK', barColor: '#EFCC18' },
+    { statName: 'DEF', barColor: '#E86412' },
+    { statName: 'SPATK', barColor: '#14C3F1' },
+    { statName: 'SPDEF', barColor: '#4A6ADF' },
+    { statName: 'SPEED', barColor: '#D51DAD' },
+  ];
+
+  // Extract stats excluding 'bst'
+  const statEntries = Object.entries(stats).filter(([key]) => key !== 'bst');
+
   const DisplayDexNumbers = ({ pokemonEntries, targetName }: DisplayDexNumbersProps) => {
     // Predefine region order
     const regionOrder = ['Paldea', 'Kitakami', 'Blueberry', 'National'];
-
-    // Format the number based on region type
-    const formatNumber = (type: string, number: string) => {
-      const padding = type === 'National' ? 4 : 3;
-      return number.padStart(padding, '0');
-    };
 
     // Filter and sort entries
     const sortedEntries = pokemonEntries
@@ -96,7 +115,7 @@ const PokeDetails = ({ route }: any) => {
     const displayText = sortedEntries
       .map(
         (entry) =>
-          `${entry.dex_info.type} #${formatNumber(entry.dex_info.type, entry.dex_info.number)}`
+          `${entry.dex_info.type} #${entry.dex_info.number}`
       )
       .join(', ');
 
@@ -107,15 +126,12 @@ const PokeDetails = ({ route }: any) => {
     );
   };
 
-  // Pokemon Type Component
   const DisplayPokemonTypes = ({ types }: { types: Types[] }) => {
     return (
       <View style={styles.typeContainer}>
         {types.map((type) => {
-          // Access the image for each type
           const typeImage = pokemonTypes[type];
-
-          if (!typeImage) return null; // In case the type doesn't have an image
+          if (!typeImage) return null;
 
           return (
             <View key={type} style={styles.typeRow}>
@@ -128,26 +144,16 @@ const PokeDetails = ({ route }: any) => {
     );
   };
 
-  // Pokemon Bar Colors
-  const barData = [
-    { statName: 'HP', barColor: '#69DC12', backgroundColor: '#9EE865' },
-    { statName: 'ATK', barColor: '#EFCC18', backgroundColor: '#F5DE69' },
-    { statName: 'DEF', barColor: '#E86412', backgroundColor: '#F09A65' },
-    { statName: 'SPATK', barColor: '#14C3F1', backgroundColor: '#66D8F6' },
-    { statName: 'SPDEF', barColor: '#4A6ADF', backgroundColor: '#899EEA' },
-    { statName: 'SPEED', barColor: '#D51DAD', backgroundColor: '#E46CCA' },
-  ];
-
-  // Extract stats excluding 'bst'
-  const statEntries = Object.entries(stats).filter(([key]) => key !== 'bst');
-
   return (
     <View style={styles.container}>
+      {/* Basic Pokemon Information */}
       <DisplayDexNumbers pokemonEntries={pokeData} targetName={pokemon.pokemon_info.name} />
       <Text style={styles.pokemonName}>{pokemon.pokemon_info.name}</Text>
       <DisplayPokemonTypes types={pokemon.pokemon_info.type} />
       <Text style={styles.abilityLabel}>Abilities:</Text>
       <Text>{pokemon.pokemon_info.abilities.join(', ')}</Text>
+  
+      {/* Base Stat Information */}
       <Text style={styles.label}>Base Stats:</Text>
       {statEntries.map(([statName, statValue], index) => (
         <StatBar
@@ -155,14 +161,47 @@ const PokeDetails = ({ route }: any) => {
           statName={statName}
           statValue={statValue as number}
           barColor={barData[index]?.barColor || '#4CAF50'}
-          backgroundColor={barData[index]?.backgroundColor || '#e0e0e0'}
         />
       ))}
       <View style={styles.totalContainer}>
         <Text style={styles.total}>Total:</Text>
         <Text style={styles.totalValue}>{pokemon.pokemon_info.stats.bst}</Text>
       </View>
-
+  
+      {/* Pokemon Learnset Information */}
+      <View style={styles.learnsetContainer}>
+        <TouchableOpacity
+          style={styles.learnsetHeader}
+          onPress={() => setIsCollapsed(!isCollapsed)}
+        >
+          <Text style={styles.learnsetHeaderText}>Pokemon Learnset</Text>
+          <Icon
+            name={isCollapsed ? 'expand-more' : 'expand-less'} // Chevron up/down based on state
+            size={24}
+            color="#000"
+          />
+        </TouchableOpacity>
+        <Collapsible collapsed={isCollapsed}>
+          <View style={{ padding: 10, backgroundColor: 'lightgray' }}>
+            <View style={styles.tableContainer}>
+              <View style={styles.tableHeader}>
+                <Text style={styles.columnHeader}>Level</Text>
+                <Text style={styles.columnHeader}>Move</Text>
+              </View>
+              <FlatList
+                data={flattenedMoves}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                  <View style={styles.tableRow}>
+                    <Text style={styles.cell}>{item.level}</Text>
+                    <Text style={styles.cell}>{item.move}</Text>
+                  </View>
+                )}
+              />
+            </View>
+          </View>
+        </Collapsible>
+      </View>
     </View>
   );
 };
@@ -249,6 +288,49 @@ const styles = StyleSheet.create({
     width: 50,
     fontWeight: '500',
     textAlign: 'right',
+  },
+  learnsetContainer: {
+    padding: 16,
+  },
+  learnsetHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+  },
+  learnsetHeaderText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  tableContainer: {
+    marginTop: 10,
+    backgroundColor: '#e6e6e6',
+    padding: 10,
+    borderRadius: 5,
+  },
+  tableHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: '#333',
+    paddingBottom: 5,
+    marginBottom: 5,
+  },
+  columnHeader: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 5,
+  },
+  cell: {
+    flex: 1,
+    fontSize: 16,
   },
 });
 
